f24aa32ecaee633238f8f116933e56a471fefcf4
Katerina Paparizou <76427845+Katerinajames@users.noreply.github.com>
2022-07-22 00:46:26 +0300

    Add files via upload

---
+++ page_80_first_decrypt.py
@@ -0,0 +1,21 @@
+
+m="daed era meht fo owt fi terces a peek nac eerhT"
+
+
+translated="  "
+i = len(m)-1
+
+print("The length of our message is",i)
+
+print("------------------------------")
+
+while i>=0:
+	translated=translated+m[i]
+	i=i-1
+	
+    
+
+print("-------------------------")
+print(translated)
+
+
---
+++ page_80_first_encrypt.py
@@ -0,0 +1,20 @@
+
+m="Three can keep a secret if two of them are dead"
+
+translated="  "
+i = len(m)-1
+
+print("The length of our message is",i)
+
+print("------------------------------")
+
+while i>=0:
+	translated=translated+m[i]
+	i=i-1
+	
+    
+
+print("-------------------------")
+print(translated)
+
+
